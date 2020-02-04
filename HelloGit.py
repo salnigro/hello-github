fcbb12d90ed0cc6f677f2c7d35a6af56baea8e33
salnigro <salnigro627@gmail.com>
2020-02-04 08:36:18 -0600

    SYntax notes

--- HelloGit.py
+++ HelloGit.py
@@ -12,3 +12,34 @@ def sumFunc(a,b):
 	return a + b
 
 print(sumFunc(2,20))
+
+phrase = 'catch the dog'
+phrase[2]
+phrase[4]
+phrase[-1: 0: -3]
+
+x = 10
+y = -10
+
+if(x==10):
+	x = 5
+elif(y==-10):
+	y = 5
+else:
+	x=y-x
+
+myList = [1,3,8,412,43,2,20]
+
+len(myList)
+
+for i in range(len(myList)):
+	print(myList)
+
+for val in myList:
+	print(val)
+
+lookup = {}
+lookup['kc'] = 'chiefs'
+lookup['ne'] = 'patriots'
+lookup['la'] = 'chargers'
+lookup['suck'] = 'raiders'
